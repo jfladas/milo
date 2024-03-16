784fc9d814f63ebe2042166a4a289025f475b2a9
Lukas Bucher <lukas.bucherr@gmail.com>
2024-03-16 23:43:56 +0100

    added parallax scrolling effect

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 let scrl = false;
 let rev = false;
+let end = false;
+let nxttxt = null;
 let scrlSpeed = 1;
 let started = false;
 let fullscreen = false;
 let iconfs = document.getElementById("iconfs");
-
-function start() {
-    started = true;
-    document.getElementById("uicon").style.background = "none";
-    document.getElementById("uiimg").style.display = "none";
-}
-
-//start();
 
 // Fox factory function
 const createFox = (elem, skin) => {
@@ -32,8 +26,8 @@ const createFox = (elem, skin) => {
             { clip: 'rect(0px, 256px, 256px, 0px)', transform: 'translate(' + x + 'px, 0px)' },
             { clip: 'rect(0px, 256px, 256px, 0px)', transform: 'translate(' + x + 'px, 0px)' }
         ];
-        if (scrl && !(rev && bg.getX() >= 0) && !(!rev && bg.getX() <= window.innerWidth * (-1) + 256)) {
-            foxSpeed = window.innerWidth / 300;
+        if (scrl && (!(rev && bg.getX() >= 0) || !(rev && x <= 0)) && (!(!rev && bg.getX() <= window.innerWidth * (-1) + 256) || !(!rev && x >= window.innerWidth - 512))) {
+            foxSpeed = (window.innerWidth - 512) / 300;
             if ((rev && x <= 0) || (!rev && x >= window.innerWidth - 512)) {
                 foxSpeed = 0;
             }
@@ -55,6 +49,7 @@ const createFox = (elem, skin) => {
 
         foxAnim.effect.setKeyframes(keyframes);
         foxAnim.play();
+        checkEnd();
     };
 
     foxAnim.onfinish = animateFox;
@@ -83,7 +78,7 @@ const createSpark = (elem, skin) => {
             { clip: 'rect(0px, 384px, 96px, 288px)', transform: 'translate(-288px, 0px)' },
             { clip: 'rect(0px, 96px, 96px, 0px)', transform: 'translate(0px, 0px)' }
         ];
-
+        document.getElementById("sparkSkin").style.zIndex = "5";
         sparkAnim.effect.setKeyframes(keyframes);
         sparkAnim.play();
     };
@@ -108,7 +103,7 @@ const createBg = (elem) => {
             x = window.innerWidth * (-1) + 256;
         }
         if (scrl && !(rev && x >= 0) && !(!rev && x <= window.innerWidth * (-1) + 256)) {
-            bgSpeed = window.innerWidth / 500;
+            bgSpeed = window.innerWidth / 600;
             if (rev) {
                 x += bgSpeed*scrlSpeed;
             } else {
@@ -120,6 +115,10 @@ const createBg = (elem) => {
             { transform: 'translateX(' + x + 'px)' }
         ]);
         bgAnim.play();
+        prlxItems.forEach(el => {
+            el.animatePrlx(x);
+        });
+        checkEnd();
     };
 
     bgAnim.onfinish = animateBG;
@@ -130,13 +129,87 @@ const createBg = (elem) => {
     };
 };
 
+// Background factory function
+const createPrlx = (elem, factor, pos) => {
+    let p = pos;
+    const prlxAnim = elem.animate(null, { duration: 10, easing: 'steps(10)' });
+
+    const animatePrlx = (x) => {
+        x = x * parseFloat(factor) + parseInt(p);
+        prlxAnim.effect.setKeyframes([
+            prlxAnim.effect.getKeyframes()[1],
+            { transform: 'translateX(' + x + 'px)' }
+        ]);
+        
+        prlxAnim.play();
+    };
+
+    return {
+        animatePrlx
+    };
+};
+
 // Usage of the factory functions
 const fox = createFox(document.getElementById("fox"), document.getElementById("foxSkin"));
 const spark = createSpark(document.getElementById("spark"), document.getElementById("sparkSkin"));
 const bg = createBg(document.getElementById("bg"));
 
-let dialogue = ["Hello", "How are you?", "What's your name?What's your name?What's your name?What's your name?What's your name?What's your name?What's your name?What's your name?What's your name?What's your name?"];
-let progress = 0;
+let prlxItems = [];
+let els = document.getElementsByClassName("prlx");
+Array.prototype.forEach.call(els, function (el) {
+    prlxItems.push(createPrlx(el, el.getAttribute("data-prlx-factor"), el.getAttribute("data-prlx-pos")));
+});
+
+function start() {
+    started = true;
+    document.getElementById("uicon").style.background = "none";
+    document.getElementById("uiimg").style.display = "none";
+    document.getElementById("uicon").style.zIndex = "0";
+}
+
+function checkEnd() {
+    if (bg.getX() == window.innerWidth * (-1) + 256 && fox.getX() == window.innerWidth - 512) {
+        end = true
+    } else {
+        end = false;
+    }
+}
+
+// Dialogue factory function
+const createDialogue = (script) => {
+    let progress = 0;
+
+    function next() {
+        if (progress >= script.length) {
+            return null; // End of dialogue
+        }
+
+        const current = script[progress];
+        progress++;
+
+        if (current.statement) {
+            return { type: "statement", text: current.statement };
+        } else if (current.question) {
+            return {
+                type: "question",
+                text: current.question,
+                answers: current.answers
+            };
+        }
+    }
+
+    return {
+        next,
+        getLength: () => script.length,
+        getProgress: () => progress
+    };
+};
+
+const introDia = createDialogue([
+    { statement: "Hello" },
+    { statement: "How are you?" }
+    //,{ question: "What's your name?", answers: ["John", "Alice"] }
+]);
 
 const scrollContainer = document.querySelector("main");
 let wheelEventEndTimeout = null;
@@ -154,7 +227,7 @@ window.addEventListener('wheel', (evt) => {
         if (evt.deltaY == 100 || evt.deltaY == -100) {
             scrlSpeed = 1;
         } else if (evt.deltaY > 80 || evt.deltaY < -80) {
-            scrlSpeed = 0.5;
+            scrlSpeed = 0.7;
         } else {
             scrlSpeed = 0.3;
             if (evt.deltaY > -20 && evt.deltaY < 20) {
@@ -173,23 +246,61 @@ window.addEventListener('wheel', (evt) => {
 });
 
 window.addEventListener('click', (evt) => {
-    document.getElementById("sparkSkin").style.zIndex = "5";
-    spark.animateSpark(evt.clientX, evt.clientY);
-    console.log(evt.target.id);
-    if(evt.target.id != "iconfs") {
-        if (started) {
-
-        } else {
-            if (progress < dialogue.length) {
-                document.getElementById("uitxt").innerText = dialogue[progress];
-                progress++;
-            } else {
-                document.getElementById("uitxt").innerText = "";
-                start();
+    
+    console.log("clicked on: " + evt.target.id);
+    switch (evt.target.id) {
+        case "foxSkin":
+            spark.animateSpark(evt.clientX, evt.clientY);
+            console.log("clicked on fox");
+            break;
+        case "iconfs":
+            spark.animateSpark(evt.clientX, evt.clientY);
+            toggleFullscreen();
+            break;
+        case "shrum1":
+            console.log(evt.clientY);
+            if (evt.clientY > window.innerHeight - 300) {
+                spark.animateSpark(evt.clientX, evt.clientY);
             }
+            break;
+        case "shrum2":
+            if (evt.clientY > window.innerHeight - 300) {
+                spark.animateSpark(evt.clientX, evt.clientY);
+            }
+            break;
+        case "shrum3":
+            if (evt.clientY > window.innerHeight - 300) {
+                spark.animateSpark(evt.clientX, evt.clientY);
+            }
+            break;
+        default:
+            if (!started) {
+                spark.animateSpark(evt.clientX, evt.clientY);
+            }
+    }
+    if (!started && evt.target.id != "iconfs") {
+        nxttxt = introDia.next();
+        if (nxttxt != null) {
+            document.getElementById("uitxt").innerText = nxttxt.text;
+            if(nxttxt.type == "question") {
+                for (let i = 0; i < nxttxt.answers.length; i++) {
+                    let btn = document.createElement("button");
+                    btn.innerText = nxttxt.answers[i];
+                    btn.id = "btn" + i;
+                    btn.onclick = function() {
+                        document.getElementById("uitxt").innerText = "";
+                        nxttxt = introDia.next();
+                        if (nxttxt != null) {
+                            document.getElementById("uitxt").innerText = nxttxt.text;
+                        }
+                    }
+                    document.getElementById("uicon").appendChild(btn);
+                }
+            }
+        } else {
+            document.getElementById("uitxt").innerText = "";
+            start();
         }
-    } else {
-        toggleFullscreen();
     }
     
 });
